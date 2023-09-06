8c0a99ddc24e13b810369974fd5ee494c140f2a6
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-09-06 16:16:17 +0500

    Update Splash.py

--- Splash.py
+++ Splash.py
@@ -77,7 +77,7 @@ def update_progress():
 # Timer setup
 timer = QTimer()
 timer.timeout.connect(update_progress)
-timer.start(300)  # 300 milliseconds
+timer.start(100)  # 100 milliseconds
 
 # Show window
 window.showFullScreen()
