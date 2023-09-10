f0bed395ef22c290e992104c6f818045c9a14b58
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-09-10 15:37:19 +0500

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -561,7 +561,7 @@ class workWindow(JobsMainWindow):
                 '<html><head><meta name="qrichtext" content="1" /><style type="text/css">\n'
                 "p, li { white-space: pre-wrap; }\n"
                 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-                '<p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:18pt; font-weight:600;">' + self.search_keyword + '</span></p></body></html>',
+                '<p align="center" style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" font-size:16pt; font-weight:600;">' + self.search_keyword + '</span></p></body></html>',
             )
         )
 
