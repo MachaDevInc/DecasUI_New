a77e6fbf0b1a1346c484fc72b64c26bd4d267dfb
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2023-09-16 19:12:22 +0500

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -1355,17 +1355,17 @@ class ScanThread(QThread):
 
         while self._isScanning:
             try:
-                # data = self.ser.readline().decode("utf-8").strip()
-                # print("\nSerial Data: " + data + "\n")
-                # if data != "31":
                 data = self.ser.readline().decode("utf-8").strip()
                 print("\nSerial Data: " + data + "\n")
-                if data and "31" not in data:
+                if data != "31":
+                    data = self.ser.readline().decode("utf-8").strip()
                     print("\nSerial Data: " + data + "\n")
-                    self.blink_and_sleep(self.qr_blinker)
-                    self.foundUserID.emit(data)
-                    self.scanned = True
-                    self.ser.write(self.stop_scan_command_bytes)
+                    if data and "31" not in data:
+                        print("\nSerial Data: " + data + "\n")
+                        self.blink_and_sleep(self.qr_blinker)
+                        self.foundUserID.emit(data)
+                        self.scanned = True
+                        self.ser.write(self.stop_scan_command_bytes)
 
                 uid = self.pn532.read_passive_target(timeout=0.1)
                 if uid is not None:
