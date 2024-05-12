4a9916ceeb3362b4eb1aa1f2d3bea7431678a5a6
MachaDevInc <125951486+MachaDevInc@users.noreply.github.com>
2024-05-12 15:16:07 +0500

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -2215,10 +2215,10 @@ class ScanningWindow(QMainWindow, Ui_MainWindow3):
                 self.timer.start(3000)
                 time_module.sleep(3)
 
-        try:
-            subprocess.run(["sudo", "rm", self.file_path], check=True)
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred: {e}")
+        # try:
+        #     subprocess.run(["sudo", "rm", self.file_path], check=True)
+        # except subprocess.CalledProcessError as e:
+        #     print(f"An error occurred: {e}")
         print("\nself.is_scanning_opened: ")
         print(self.is_scanning_opened)
         print("\n")
